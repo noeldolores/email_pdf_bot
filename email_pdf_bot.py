4754338e3178786c7e07d753652443292e4f5843
noeldolores <noel.dolore@gmail.com>
2021-03-21 15:28:18 -0700

    Added basic email api interactions

--- email_pdf_bot.py
+++ email_pdf_bot.py
@@ -1,1 +1,82 @@
 #!/usr/bin/env python3
+
+import emails
+import pickle
+import os
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    #print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    #print(SCOPES)
+
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    #print(pickle_file)
+
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, 'connection successful')
+        return service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
+        return None
+
+def main():
+  # Create Service Instance
+  CLIENT_SECRET_FILE = 'client_secret.json'
+  API_NAME = 'gmail'
+  API_VERSION = 'v1'
+  SCOPES = ['https://mail.google.com/']
+  
+  service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+  
+  # Send Email
+  #userID= 'me'
+  #receiver= 'noel.dolores@gmail.com'
+  #subject= 'test3'
+  #message= 'test3'
+  #attachments = ['README.md']
+  #emails.Send_Message_With_Attchments(service, userID, receiver, subject, message, attachments)
+  
+  # Check for Unread Emails
+  message_info_list = emails.Get_Unread_Messages(service, 'me')
+  for message in message_info_list:
+  #  emails.Get_Attachments(service, 'me', message, "tmp/attachments/")
+    info = emails.Get_Message_Info(service, 'me', message)
+    print("sender: " + info[0])
+    print("thread id: " + info[1])
+    print("message id: " + info[2])
+    userID= 'me'
+    receiver= info[0]
+    subject= info[3]
+    message= 'test3'
+    attachments = ['README.md']
+    threadId = info[1]
+    message_id = info[2]
+    emails.Reply_With_Attchments(service, userID, receiver, subject, message, attachments, threadId, message_id)
+
+if __name__ == "__main__":
+  main()
---
+++ emails.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+import base64
+from apiclient import errors
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import mimetypes
+import re
+
+def Get_Attachments(service, user_id, msg_id, store_dir):
+    """Get and store attachment from Message with given id.
+        Args:
+            service: Authorized Gmail API service instance.
+            user_id: User's email address. The special value "me"
+                can be used to indicate the authenticated user.
+            msg_id: ID of Message containing attachment.
+            store_dir: The directory used to store attachments.
+    """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+        parts = [message['payload']]
+        while parts:
+            part = parts.pop()
+            if part.get('parts'):
+                parts.extend(part['parts'])
+            if part.get('filename'):
+                if 'data' in part['body']:
+                    file_data = base64.urlsafe_b64decode(part['body']['data'].encode('UTF-8'))
+                    #self.stdout.write('FileData for %s, %s found! size: %s' % (message['id'], part['filename'], part['size']))
+                elif 'attachmentId' in part['body']:
+                    attachment = service.users().messages().attachments().get(
+                        userId=user_id, messageId=message['id'], id=part['body']['attachmentId']
+                    ).execute()
+                    file_data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
+                    #self.stdout.write('FileData for %s, %s found! size: %s' % (message['id'], part['filename'], attachment['size']))
+                else:
+                    file_data = None
+                if file_data:
+                    #do some staff, e.g.
+                    path = ''.join([store_dir, part['filename']])
+                    with open(path, 'wb') as f:
+                        f.write(file_data)
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+def Send_Message_With_Attchments(service, userID, receiver, subject, message, attachments): #working
+  # Create email message
+  emailMsg = message
+  mimeMessage = MIMEMultipart()
+  mimeMessage['to'] = receiver
+  mimeMessage['subject'] = subject
+  mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+  
+  # Attach files
+  file_attachments = attachments
+
+  for attachment in file_attachments:
+      content_type, encoding = mimetypes.guess_type(attachment)
+      main_type, sub_type = content_type.split('/', 1)
+      file_name = os.path.basename(attachment)
+  
+      f = open(attachment, 'rb')
+  
+      myFile = MIMEBase(main_type, sub_type)
+      myFile.set_payload(f.read())
+      myFile.add_header('Content-Disposition', 'attachment', filename=file_name)
+      encoders.encode_base64(myFile)
+  
+      f.close()
+  
+      mimeMessage.attach(myFile)
+  
+  raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+  
+  message = service.users().messages().send(
+      userId=userID,
+      body={'raw': raw_string}).execute()
+
+def Reply_With_Attchments(service, userID, receiver, subject, message, attachments, threadId, message_id):
+  # Create email message
+  emailMsg = message
+  mimeMessage = MIMEMultipart()
+  mimeMessage['to'] = receiver
+  mimeMessage['subject'] = subject
+  #mimeMessage['threadId'] = threadId
+  #mimeMessage['In-Reply-To'] = message_id
+  mimeMessage.add_header('In-Reply-To', message_id)
+  mimeMessage.add_header('References', message_id)
+  #mimeMessage['References'] = message_id
+  mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+  
+  # Attach files
+  file_attachments = attachments
+
+  for attachment in file_attachments:
+      content_type, encoding = mimetypes.guess_type(attachment)
+      main_type, sub_type = content_type.split('/', 1)
+      file_name = os.path.basename(attachment)
+  
+      f = open(attachment, 'rb')
+  
+      myFile = MIMEBase(main_type, sub_type)
+      myFile.set_payload(f.read())
+      myFile.add_header('Content-Disposition', 'attachment', filename=file_name)
+      encoders.encode_base64(myFile)
+  
+      f.close()
+  
+      mimeMessage.attach(myFile)
+  
+  raw_string = {'raw':base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()}
+  raw_string['threadId']=threadId
+  
+  message = service.users().messages().send(
+      userId=userID,
+      body=raw_string).execute()
+
+def Get_Unread_Messages(service, userId): #working
+  message_list = []
+  message_ids = service.users().messages().list(userId=userId, labelIds='INBOX', alt="json", q='is:unread has:attachment').execute()
+
+  for message in message_ids['messages']:
+    #print(message)
+    message_list.append(message['id'])
+
+  return message_list
+
+def Get_Message_Info(service, userId, message_id):
+  message_info = service.users().messages().get(userId=userId, id=message_id).execute()
+  #message_info = message_info['payload']['headers'][16]['value']
+  sender_index = 16
+  message_id_index = 18
+  subject_index = 19
+  sender = re.search(r"[<](.*)[>]", message_info['payload']['headers'][sender_index]['value'])
+  thread_id = message_info['threadId']
+  message_id = re.search(r"[<](.*)[>]", message_info['payload']['headers'][message_id_index]['value'])
+  subject = message_info['payload']['headers'][subject_index]['value']
+  info = (sender[1], thread_id, message_id[1], subject)
+  return info
